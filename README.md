6be00f6da73ee499f720e92ef087d58ac60bba3c
Aristide Sabi <sabiarist@gmail.com>
2021-12-08 16:29:04 +0000

    AI Radio project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# ai_radio
+
+AI - Powered Voice Assistant Flutter Radio App
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:ai_radio/utils/ai_util.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
